76c7a381b45d388711202a2ab96021529761d7d4
Nisha <hayaazashraf@gmail.com>
2024-07-23 20:55:59 -0700

    add Text.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Text from './Text';
+function App(){
+ return(
+ <>
+ Application
+ <Text/>
+ </>
+ );
+}
+export default App;
---
+++ src/Text.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+class Text extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return<p>Hello Text</p>;
+    }
+}
+export default Text;
